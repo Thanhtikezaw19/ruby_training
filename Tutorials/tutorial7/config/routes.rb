16bdbba5852fc46aeea20a53f61215662824eb9b
Thanhtikezaw19 <fabiomahorni@gmail.com>
2021-12-27 20:17:41 +0630

    [Tutorials] tutorial7

---
+++ Tutorials/tutorial7/config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  get '/pages', to: "pages#qr_code_generator"
+
+  post 'qr_code_download', to: 'pages#qr_code_download'
+
+  controller :pages do
+    get :qr_code_generator
+    get :qr_code_download
+  end
+
+  
+  
+end
