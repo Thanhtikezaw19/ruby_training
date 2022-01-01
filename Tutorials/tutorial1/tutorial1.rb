56afff0666a50e852632b6ffdf23c94a05c0c817
Thanhtikezaw19 <fabiomahorni@gmail.com>
2022-01-01 14:41:46 +0630

    [Tutorials] tutorial1 feedback fixed

--- Tutorials/tutorial1/tutorial1.rb
+++ Tutorials/tutorial1/tutorial1.rb
@@ -1,11 +1,16 @@
 for row in 1..8 do 
+
   for col in 1..8 do 
+
     if (col + row).even?
       print "1 "
-      else 
-        print "0 "
+    else 
+      print "0 "
     end
+    
   end
+
   puts
+
 end  
 
